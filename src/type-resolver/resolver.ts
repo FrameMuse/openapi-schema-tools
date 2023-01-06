332b558ba4852bdb45c08d90ba6b280e63226945
FrameMuse <minicablestone@gmail.com>
2023-01-06 10:43:19 +0300

    updated `ResolveSchema` added tests

--- src/type-resolver/resolver.ts
+++ src/type-resolver/resolver.ts
@@ -1,4 +1,3 @@
-
 import { SetRequired, UnionToIntersection } from "type-fest"
 import { SimplifyDeep } from "type-fest/source/merge-deep"
 
@@ -43,29 +42,27 @@ type ResolveSchema<S extends SchemaAny, Context extends Record<string, Schema> =
   : S["type"] extends ("number" | "integer") ? number
   : S["type"] extends "boolean" ? boolean
   : S["type"] extends "array" ? (S["items"] extends {} ? ResolveSchema<S["items"], Context>[] : never[])
-  : S["type"] extends "object" ? SimplifyDeep<(
-    SetRequired<{ [K in keyof NonNullable<S["properties"]>]?: ResolveSchema<NonNullable<S["properties"]>[K], Context> }, ArrayType<S["required"]>>
-  ) & S["default"]>
+  : S["type"] extends "object" ? (
+    S["properties"] extends {} ? SimplifyDeep<(
+      SetRequired<{ [K in keyof S["properties"]]?: ResolveSchema<S["properties"][K], Context> }, ArrayType<S["required"]>>
+    ) & S["default"]>
+    : {}
+  )
 
   : S extends ({ $ref: string }) ? ResolveSchema<DeRefSchema<S, Context>, Context>
   : S extends ({ anyOf: SchemaRef[] }) ? ResolveSchema<ArrayType<S["anyOf"]>, Context>
   : S extends ({ oneOf: SchemaRef[] }) ? ResolveSchema<ArrayType<S["oneOf"]>, Context>
   : S extends ({ allOf: SchemaRef[] }) ? UnionToIntersection<ResolveSchema<ArrayType<S["allOf"]>, Context>>
   : never
-// type ParseSchemaEnhance<S, O> = S extends Schema ? (O | IfNullable<S>) : never
-// type ParseSchema__TEST__ = ResolveSchema<SwaggerSchema["components"]["schemas"]["AccountsMe"]>
-// type ParseSchema__TEST__ = ResolveSchema<{ type: "string" }>
-// type ParseSchema__TEST__ = ResolveSchema<{ type: "object", properties: { foo: { type: "string" } } }>
-
-// Schema parser helpers
-
-// type ParseSchemaArray<S extends SchemaArray> = ResolveSchema<S["items"]>[]
-// type ParseSchemaNumber<S extends SchemaNumber> = number
-// type ParseSchemaString<S extends SchemaString> = string
-// type ParseSchemaBoolean<S extends SchemaBoolean> = boolean
-
-// type ParseSchemaAllOf<S> = UnionToIntersection<S extends ({ allOf: (infer T)[] }) ? ResolveSchema<T> : never>
-// // type ParseSchemaAllOf<S extends SchemaAllOf> = ResolveSchema<DeRefSchema<ArrayType<S["allOf"]> extends SchemaRef ? ArrayType<S["allOf"]> : never>>
-// type ParseSchemaAnyOf<S> = S extends ({ anyOf: (infer T)[] }) ? "anyOf" : never
 
 export default ResolveSchema
+
+/* eslint-disable @typescript-eslint/no-unused-vars */
+
+const __ResolveSchema_String__TEST__: ResolveSchema<{ type: "string" }> = ""
+const __ResolveSchema_Number__TEST__: ResolveSchema<{ type: "number" }> = 0
+const __ResolveSchema_Integer__TEST__: ResolveSchema<{ type: "integer" }> = 0
+const __ResolveSchema_Boolean__TEST__: ResolveSchema<{ type: "boolean" }> = true
+
+const __ResolveSchema_Array__TEST__: ResolveSchema<{ type: "array", items: { type: "object", properties: { foo: { type: "string" } } } }> = [{ foo: "" }]
+const __ResolveSchema_Object__TEST__: ResolveSchema<{ type: "object", properties: { foo: { type: "string" } } }> = { foo: "" }
