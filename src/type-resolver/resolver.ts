599d273a8a0fb6e7695e5ef0264700ed8b1c4024
Valery Zinchenko <minicablestone@gmail.com>
2023-10-23 12:07:18 +0300

    fix `ResolveSchema<never>` returns `string` #1 Reworked `ResolveSchema`

--- src/type-resolver/resolver.ts
+++ src/type-resolver/resolver.ts
@@ -2,51 +2,25 @@ import { SetRequired, UnionToIntersection } from "type-fest"
 import { SimplifyDeep } from "type-fest/source/merge-deep"
 
 import { ArrayType, DeRefSchema } from "../helpers"
-import { Schema, SchemaAny, SchemaRef } from "../types"
-
-// Schema parser
-
-// type dd = ResolveSchema<{ type: "array", items: { type: "string" } }>
-
-// type asd<S> = S extends Schema ? S : never
-// type ResolveSchema<S> = ParseSchemaEnhance<S, (
-//   S extends SchemaRef ? ResolveSchema<DeRefSchema<S>> :
-//   // S extends { allOf: (infer T)[] } ? (ResolveSchema<T>) :
-//   // S extends SchemaAnyOf ? ParseSchemaAnyOf<S> :
-
-//   S extends SchemaString ? ParseSchemaString<S> :
-//   S extends SchemaNumber ? ParseSchemaNumber<S> :
-//   S extends SchemaObject ? ParseSchemaObject<S> :
-//   S extends SchemaBoolean ? ParseSchemaBoolean<S> :
-//   S extends SchemaArray ? ParseSchemaArray<S> :
-//   never
-// )>
-// type ParseSchemaEnhance<S, O> = O
-
-
-
-// Props extends Record<string, Schema> ?
-//   (MarkRequired<{ [K in keyof Props]?: ResolveSchema<Props[K]> }, ArrayType<S["required"]>> & S["default"])
-//   : never
-
-
-
+import { SchemaAny, SchemaRef, Schemas } from "../types"
 
 /**
  * Resolves **OpenAPI JSON Schema**.
  * 
  * To resolve `Schema` you may need a context if there are any `$ref`'s.
  */
-type ResolveSchema<S extends SchemaAny, Context extends Record<string, Schema> = {}> =
-  S["type"] extends "string" ? string
+type ResolveSchema<S extends SchemaAny, Context extends Schemas = {}> =
+  S extends never ? never
+  : S["type"] extends "string" ? string
   : S["type"] extends ("number" | "integer") ? number
   : S["type"] extends "boolean" ? boolean
-  : S["type"] extends "array" ? (S["items"] extends {} ? ResolveSchema<S["items"], Context>[] : never[])
+
+  : S["type"] extends "array" ? (S["items"] extends object ? ResolveSchema<S["items"], Context>[] : never[])
   : S["type"] extends "object" ? (
-    S["properties"] extends {} ? SimplifyDeep<(
+    S["properties"] extends object ? SimplifyDeep<(
       SetRequired<{ [K in keyof S["properties"]]?: ResolveSchema<S["properties"][K], Context> }, ArrayType<S["required"]>>
     )>
-    : {}
+    : Record<keyof never, unknown>
   ) & S["default"]
 
   : S extends ({ $ref: string }) ? ResolveSchema<DeRefSchema<S, Context>, Context>
@@ -63,6 +37,8 @@ const __ResolveSchema_String__TEST__: ResolveSchema<{ type: "string" }> = ""
 const __ResolveSchema_Number__TEST__: ResolveSchema<{ type: "number" }> = 0
 const __ResolveSchema_Integer__TEST__: ResolveSchema<{ type: "integer" }> = 0
 const __ResolveSchema_Boolean__TEST__: ResolveSchema<{ type: "boolean" }> = true
+
+const __ResolveSchema_Never__TEST__: never = {} as ResolveSchema<never>
 
 const __ResolveSchema_Array__TEST__: ResolveSchema<{ type: "array", items: { type: "object", properties: { foo: { type: "string" } } } }> = [{ foo: "" }]
 const __ResolveSchema_Object__TEST__: ResolveSchema<{ type: "object", properties: { foo: { type: "string" } } }> = { foo: "" }
